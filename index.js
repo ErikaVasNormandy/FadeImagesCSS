c34e1aaa7ef2b07cb06d5c37487019fc926e67d2
ASCI-Red <erika.h.2012@gmail.com>
2017-02-21 14:04:13 -0800

    cannot just do ngstyle

---
+++ index.js
@@ -0,0 +1,9 @@
+var appName=angular.module("AppForHTML", []);
+appName.controller("ctrl1", function($scope, $timeout) {
+  $scope.message="Hello!"
+  // $scope.AngularCSS={
+  //
+  //
+  // }
+
+});
